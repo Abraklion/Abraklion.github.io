83ee56e6b0d6284efd72237c92632893d4bb7f1c
Abraklion <75311365+Abraklion@users.noreply.github.com>
2020-12-11 12:14:14 +0500

    build

---
+++ project-14/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('.case-study__carousel .owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        dots: true,
+        navText: [],
+        responsive:{
+            0:{
+                items:1,
+            }
+        }
+    })
+
+});
